08cfc806f8a7d4ac1ac50bf02dd04f0008d00757
MITS-CN <mits-cn@outlook.com>
2026-03-14 02:39:17 +0800

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,12 +1,6 @@
 termux-setup-storage
 mkdir /storage/emulated/0/MITS
-<<<<<<< HEAD
 cp -r ./* /storage/emulated/0/MITS
 chmod +x /storage/emulated/0/MITS/data/bin/init.rc
 bash /storage/emulated/0/MITS/data/bin/init.rc
 rm -rf /storage/emulated/0/MITS/
-=======
-cp ./* /storage/emulated/0/MITS
-chmod +x /storage/emulated/0/MITS/data/bin/init.rc
-bash /storage/emulated/0/MITS/data/bin/init.rc
->>>>>>> 549799457e1215a1e10bfd4cd632baf5297921f2
