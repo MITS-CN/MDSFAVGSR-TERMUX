025b3d2bfa1dd077d5d083e9cd13c2eb4fdcaa3b
MITS-CN <MITS-CN@outlook.com>
2026-04-06 00:02:04 +0800

    fix

--- data/bin/temp.data.make.c.sh
+++ data/bin/temp.data.make.c.sh
@@ -1,6 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-apps=("version" "tasklist" "taskkill" "winver" "diskpart" "systeminfo" "pause" "netsh" "ver" "help" "type" "rename" "move" "ren" "744d374387517fe2c2f10f15e7a7e70797b5e")
+apps=("version" "tasklist" "taskkill" "winver" "diskpart" "systeminfo" "pause" "netsh" "ver" "help" "type" "rename" "move" "ren" "04fe76d6671ee2c9c77d7268291744d374387517fe2c2f10f15e7a7e70797b5e.cpp")
 
 #744d374387517fe2c2f10f15e7a7e70797b5e 是hash值，用于临时使用的命令
 
